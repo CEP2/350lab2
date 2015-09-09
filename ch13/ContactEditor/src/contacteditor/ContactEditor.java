a1699a0f31d87d6fcca28a9e4423931b12a320cd
cep2 <clifporter@gmail.com>
2015-09-08 22:45:31 -0700

    Adding in files for Contact Editor.

---
+++ ch13/ContactEditor/src/contacteditor/ContactEditor.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contacteditor;
+
+import javax.swing.JFrame;
+import my.contacteditor.ContactEditorUI;
+
+/**
+ * @Authors
+ * Clifton Porter
+ * Marco Antônio
+ * 
+ * Purpose
+ * Lab 2 assignment, making GUI from Swift
+ * 
+ * Additional Notes
+ * These authorship notes are added under the 
+ * branch authorship to be merged in with master.
+ * 
+ */
+public class ContactEditor extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      ContactEditorUI CON = new ContactEditorUI();
+      CON.setVisible(true);
+    }
+    
+}
